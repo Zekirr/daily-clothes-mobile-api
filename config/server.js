2dc8efbe52ab5687ad994551a5384295e0d67f16
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2022-05-11 14:31:54 +0200

    Correction erreur de session et keys

--- config/server.js
+++ config/server.js
@@ -1,7 +1,14 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
+      },
+    },
   },
 });
